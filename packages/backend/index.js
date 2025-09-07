713bc9205e9e517b9324c0882a1e3c5eecfef2ff
Brandon Vallejo <vallejobrandon4@gmail.com>
2025-09-07 16:07:29 -0300

    Configs del nuevo repo clonado

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -16,6 +16,7 @@ app.get("/hello", (req, res) => {
   res.json({ message: "hello world" });
 });
 
+
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Backend escuchando en puerto ${process.env.SERVER_PORT}`);
 });
